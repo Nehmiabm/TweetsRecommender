b996f321c750d4b12307c23e062090db2e584416
Betsu <nehmiabm@gmail.com>
2016-08-09 23:25:58 -0500

    Added weka class library for analysing datasets and implemented test analysis on iris dataset

--- TweetsRecommender/Program.cs
+++ TweetsRecommender/Program.cs
@@ -13,8 +13,13 @@ namespace TweetsRecommender
     {
         static  void Main(string[] args)
         {
-
             // Pass your credentials to the service
+          //  GenerateTweets();
+          ClassifyTest();
+        }
+
+        private static void GenerateTweets()
+        {
             string consumerKey = "BVCKULB1I0tv8zIlxuk1jSRCg";
             string consumerSecret = "owNQNPAosiQm74r3eS01iLm8o9zBbtRpkUCU4ml1tCZ5nvEsiu";
 
@@ -22,7 +27,7 @@ namespace TweetsRecommender
             string accessTokenSecret = "auBPaVFwPpkB0JG2WlsfHoUJHcn4V3obyJher9lFXUWfa";
             TwitterService service = new TwitterService(consumerKey, consumerSecret);
             StringBuilder sb = new StringBuilder();
-          
+
             service.AuthenticateWith(accessToken, accessTokenSecret);
 
             var result =
@@ -34,7 +39,7 @@ namespace TweetsRecommender
                         {
                             Radius = 1,
                             Coordinates =
-                                new TwitterGeoLocation.GeoCoordinates() 
+                                new TwitterGeoLocation.GeoCoordinates()
                                 {Latitude = 41.00688, Longitude = -91.967137} //Search by geo location for Fairfield, IA
                         }
                 });
@@ -45,13 +50,13 @@ namespace TweetsRecommender
                 string address = place == null ? "" : place.FullName;
                 Console.WriteLine("==========================================================================");
                 sb.AppendLine("==========================================================================");
-                Console.WriteLine("Author Screen Name:"+status.Author.ScreenName);
+                Console.WriteLine("Author Screen Name:" + status.Author.ScreenName);
                 sb.AppendLine("Author Screen Name:" + status.Author.ScreenName);
-                Console.WriteLine("Created Date:"+status.CreatedDate.ToLongDateString());
+                Console.WriteLine("Created Date:" + status.CreatedDate.ToLongDateString());
                 sb.AppendLine("Created Date:" + status.CreatedDate.ToLongDateString());
                 Console.WriteLine("Text:" + status.Text);
                 sb.AppendLine("Text:" + status.Text);
-                Console.WriteLine("Id:"+status.Id);
+                Console.WriteLine("Id:" + status.Id);
                 sb.AppendLine("Id:" + status.Id);
                 Console.WriteLine("Favorite Count:" + status.FavoriteCount);
                 sb.AppendLine("Favorite Count:" + status.FavoriteCount);
@@ -71,7 +76,7 @@ namespace TweetsRecommender
                 sb.AppendLine("Raw Source:" + status.RawSource);
                 Console.WriteLine("Source:" + status.Source);
                 sb.AppendLine("Source:" + status.Source);
-                Console.WriteLine("Country:" + country+", Address: "+address);
+                Console.WriteLine("Country:" + country + ", Address: " + address);
                 sb.AppendLine("Country:" + country + ", Address: " + address);
                 Console.WriteLine("User Name:" + status.User.Name);
                 sb.AppendLine("User Name:" + status.User.Name);
@@ -89,14 +94,52 @@ namespace TweetsRecommender
                 sb.AppendLine("User Id:" + status.User.Id);
                 Console.WriteLine("User Location:" + status.User.Location);
                 sb.AppendLine("User Location:" + status.User.Location);
-
             }
             if (!System.IO.Directory.Exists(@"C\bigdata"))
                 System.IO.Directory.CreateDirectory(@"C:\bigdata");
-            System.IO.File.WriteAllText(@"C:\bigdata\bigdata.txt",sb.ToString());
+            System.IO.File.WriteAllText(@"C:\bigdata\bigdata.txt", sb.ToString());
             Console.ReadKey();
-            
-          
         }
+
+     
+        public static void ClassifyTest()
+        {
+            const int percentSplit = 66;
+            try
+            {
+                weka.core.Instances insts = new weka.core.Instances(new java.io.FileReader("iris.arff"));
+                insts.setClassIndex(insts.numAttributes() - 1);
+
+                weka.classifiers.Classifier cl = new weka.classifiers.trees.J48();
+                Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
+
+                //randomize the order of the instances in the dataset.
+                weka.filters.Filter myRandom = new weka.filters.unsupervised.instance.Randomize();
+                myRandom.setInputFormat(insts);
+                insts = weka.filters.Filter.useFilter(insts, myRandom);
+
+                int trainSize = insts.numInstances() * percentSplit / 100;
+                int testSize = insts.numInstances() - trainSize;
+                weka.core.Instances train = new weka.core.Instances(insts, 0, trainSize);
+
+                cl.buildClassifier(train);
+                int numCorrect = 0;
+                for (int i = trainSize; i < insts.numInstances(); i++)
+                {
+                    weka.core.Instance currentInst = insts.instance(i);
+                    double predictedClass = cl.classifyInstance(currentInst);
+                    if (predictedClass == insts.instance(i).classValue())
+                        numCorrect++;
+                }
+                Console.WriteLine(numCorrect + " out of " + testSize + " correct (" +
+                           (double)((double)numCorrect / (double)testSize * 100.0) + "%)");
+            }
+            catch (java.lang.Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+
     }
 }
+
