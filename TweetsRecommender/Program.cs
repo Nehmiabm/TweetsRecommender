dadcc46bafb6ae4b225335e123777efdc17ae83f
Betsu <nehmiabm@gmail.com>
2016-08-11 23:27:44 -0500

    implemented POS analysis

--- TweetsRecommender/Program.cs
+++ TweetsRecommender/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
@@ -7,16 +8,45 @@ using System.Text;
 using System.Threading.Tasks;
 using ikvm.extensions;
 using TweetSharp;
+using OpenNLP.Tools;
+using OpenNLP.Tools.PosTagger;
+using OpenNLP.Tools.Tokenize;
+using weka.core;
 
 namespace TweetsRecommender
 {
     class Program
     {
-        static  void Main(string[] args)
+
+        #region Noun Constants
+
+        private const string NN = "NN"; //Noun, singular or mass 
+        private const string NNP = "NNP"; //Proper noun, singular
+        private const string NNPS = "NNPS"; //Proper noun, plural
+        private const string NNS = "NNS"; //Noun, plural
+
+        #endregion
+
+        #region Verb Constants
+
+        private const string VB = "VB"; //Verb, base form
+        private const string VBD = "VBD"; //Verb, past tense
+        private const string VBG = "VBG"; //Verb, gerund/present participle
+        private const string VBN = "VBN"; //Verb, past participle
+        private const string VBP = "VBP"; //Verb, non-3rd ps. sing. present
+        private const string VBZ = "VBZ"; //Verb, 3rd ps. sing. present
+
+        #endregion
+
+        private static StringBuilder tagBuilder = new StringBuilder();
+        private static string modelPath = ConfigurationManager.AppSettings["ModelPath"];
+        static void Main(string[] args)
         {
-           
-          GenerateTweets();
-         // ClassifyTest();
+
+           GenerateTweets();
+          // AnalyzeTweetUsingPosTest("@mellie_me12009: The more you know...She was NOT crazy...");
+            // ClassifyTest();
+           // AnalyzeTweetUsingPos("When I typed in the first part of the hashtag, #blacklives, this was #Twitter's first and only suggestion");
         }
 
         private static void GenerateTweets()
@@ -34,76 +64,81 @@ namespace TweetsRecommender
             var result =
                 service.Search(new SearchOptions()
                 {
-                    Count = 100, //Number of tweets
-                    // Q = "#BlackLives"
-                    Geocode =
-                        new TwitterGeoLocationSearch()
-                        {
-                            Radius = 1,
-                            Coordinates =
-                                new TwitterGeoLocation.GeoCoordinates()
-                                {Latitude = 41.00688, Longitude = -91.967137} //Search by geo location for Fairfield, IA
-                        }
+                    Count = 20, //Number of tweets
+                     Q = "#BlackLives"
+                    //Geocode =
+                    //    new TwitterGeoLocationSearch()
+                    //    {
+                    //        Radius = 1,
+                    //        Coordinates =
+                    //            new TwitterGeoLocation.GeoCoordinates()
+                    //            {Latitude = 41.00688, Longitude = -91.967137} //Search by geo location for Fairfield, IA
+                    //    }
                 });
             foreach (TwitterStatus status in result.Statuses)
             {
-                var place = status.Place;
-                string country = place == null ? "" : place.Country;
-                string address = place == null ? "" : place.FullName;
-                Console.WriteLine("==========================================================================");
-                sb.AppendLine("==========================================================================");
-                Console.WriteLine("Author Screen Name:" + status.Author.ScreenName);
-                sb.AppendLine("Author Screen Name:" + status.Author.ScreenName);
-                Console.WriteLine("Created Date:" + status.CreatedDate.ToLongDateString());
-                sb.AppendLine("Created Date:" + status.CreatedDate.ToLongDateString());
-                Console.WriteLine("Text:" + status.Text);
-                sb.AppendLine("Text:" + status.Text);
-                Console.WriteLine("Id:" + status.Id);
-                sb.AppendLine("Id:" + status.Id);
-                Console.WriteLine("Favorite Count:" + status.FavoriteCount);
-                sb.AppendLine("Favorite Count:" + status.FavoriteCount);
-                Console.WriteLine("ID String:" + status.IdStr);
-                sb.AppendLine("ID String:" + status.IdStr);
-                Console.WriteLine("In Reply To Screen Name:" + status.InReplyToScreenName);
-                sb.AppendLine("In Reply To Screen Name:" + status.InReplyToScreenName);
-                Console.WriteLine("Is Retweeted:" + status.IsRetweeted);
-                sb.AppendLine("Is Retweeted:" + status.IsRetweeted);
-                Console.WriteLine("Is Favorited:" + status.IsFavorited);
-                sb.AppendLine("Is Favorited:" + status.IsFavorited);
-                Console.WriteLine("Language:" + status.Language);
-                sb.AppendLine("Language:" + status.Language);
-                Console.WriteLine("Retweet Count:" + status.RetweetCount);
-                sb.AppendLine("Retweet Count:" + status.RetweetCount);
-                Console.WriteLine("Raw Source:" + status.RawSource);
-                sb.AppendLine("Raw Source:" + status.RawSource);
-                Console.WriteLine("Source:" + status.Source);
-                sb.AppendLine("Source:" + status.Source);
-                Console.WriteLine("Country:" + country + ", Address: " + address);
-                sb.AppendLine("Country:" + country + ", Address: " + address);
-                Console.WriteLine("User Name:" + status.User.Name);
-                sb.AppendLine("User Name:" + status.User.Name);
-                Console.WriteLine("User Email:" + status.User.Email);
-                sb.AppendLine("User Email:" + status.User.Email);
-                Console.WriteLine("User Description:" + status.User.Description);
-                sb.AppendLine("User Description:" + status.User.Description);
-                Console.WriteLine("User Followers Count:" + status.User.FollowersCount);
-                sb.AppendLine("User Followers Count:" + status.User.FollowersCount);
-                Console.WriteLine("User Created Date:" + status.User.CreatedDate.ToLongDateString());
-                sb.AppendLine("User Created Date:" + status.User.CreatedDate.ToLongDateString());
-                Console.WriteLine("User Friends Count:" + status.User.FriendsCount);
-                sb.AppendLine("User Friends Count:" + status.User.FriendsCount);
-                Console.WriteLine("User Id:" + status.User.Id);
-                sb.AppendLine("User Id:" + status.User.Id);
-                Console.WriteLine("User Location:" + status.User.Location);
-                sb.AppendLine("User Location:" + status.User.Location);
+                if (status.Language.Equals("en"))
+                {
+                    var place = status.Place;
+                    string country = place == null ? "" : place.Country;
+                    string address = place == null ? "" : place.FullName;
+                    Console.WriteLine("==========================================================================");
+                    sb.AppendLine("==========================================================================");
+                    Console.WriteLine("Author Screen Name:" + status.Author.ScreenName);
+                    sb.AppendLine("Author Screen Name:" + status.Author.ScreenName);
+                    Console.WriteLine("Created Date:" + status.CreatedDate.ToLongDateString());
+                    sb.AppendLine("Created Date:" + status.CreatedDate.ToLongDateString());
+                    Console.WriteLine("Text:" + status.Text);
+                    AnalyzeTweetUsingPos(status.Text);
+                    sb.AppendLine("Text:" + status.Text);
+                    Console.WriteLine("Id:" + status.Id);
+                    sb.AppendLine("Id:" + status.Id);
+                    Console.WriteLine("Favorite Count:" + status.FavoriteCount);
+                    sb.AppendLine("Favorite Count:" + status.FavoriteCount);
+                    Console.WriteLine("ID String:" + status.IdStr);
+                    sb.AppendLine("ID String:" + status.IdStr);
+                    Console.WriteLine("In Reply To Screen Name:" + status.InReplyToScreenName);
+                    sb.AppendLine("In Reply To Screen Name:" + status.InReplyToScreenName);
+                    Console.WriteLine("Is Retweeted:" + status.IsRetweeted);
+                    sb.AppendLine("Is Retweeted:" + status.IsRetweeted);
+                    Console.WriteLine("Is Favorited:" + status.IsFavorited);
+                    sb.AppendLine("Is Favorited:" + status.IsFavorited);
+                    Console.WriteLine("Language:" + status.Language);
+                    sb.AppendLine("Language:" + status.Language);
+                    Console.WriteLine("Retweet Count:" + status.RetweetCount);
+                    sb.AppendLine("Retweet Count:" + status.RetweetCount);
+                    Console.WriteLine("Raw Source:" + status.RawSource);
+                    sb.AppendLine("Raw Source:" + status.RawSource);
+                    Console.WriteLine("Source:" + status.Source);
+                    sb.AppendLine("Source:" + status.Source);
+                    Console.WriteLine("Country:" + country + ", Address: " + address);
+                    sb.AppendLine("Country:" + country + ", Address: " + address);
+                    Console.WriteLine("User Name:" + status.User.Name);
+                    sb.AppendLine("User Name:" + status.User.Name);
+                    Console.WriteLine("User Email:" + status.User.Email);
+                    sb.AppendLine("User Email:" + status.User.Email);
+                    Console.WriteLine("User Description:" + status.User.Description);
+                    sb.AppendLine("User Description:" + status.User.Description);
+                    Console.WriteLine("User Followers Count:" + status.User.FollowersCount);
+                    sb.AppendLine("User Followers Count:" + status.User.FollowersCount);
+                    Console.WriteLine("User Created Date:" + status.User.CreatedDate.ToLongDateString());
+                    sb.AppendLine("User Created Date:" + status.User.CreatedDate.ToLongDateString());
+                    Console.WriteLine("User Friends Count:" + status.User.FriendsCount);
+                    sb.AppendLine("User Friends Count:" + status.User.FriendsCount);
+                    Console.WriteLine("User Id:" + status.User.Id);
+                    sb.AppendLine("User Id:" + status.User.Id);
+                    Console.WriteLine("User Location:" + status.User.Location);
+                    sb.AppendLine("User Location:" + status.User.Location);
+                }
             }
             if (!System.IO.Directory.Exists(@"C\bigdata"))
                 System.IO.Directory.CreateDirectory(@"C:\bigdata");
             System.IO.File.WriteAllText(@"C:\bigdata\bigdata.txt", sb.ToString());
+            System.IO.File.WriteAllText(@"C:\bigdata\tagslist.txt", tagBuilder.toString());
             Console.ReadKey();
         }
 
-     
+
         public static void ClassifyTest()
         {
             const int percentSplit = 66;
@@ -114,21 +149,21 @@ namespace TweetsRecommender
 
                 weka.classifiers.Classifier cl = new weka.classifiers.trees.J48();
                 Console.WriteLine("Performing " + percentSplit + "% split evaluation.");
-               
+
                 //randomize the order of the instances in the dataset.
                 weka.filters.Filter myRandom = new weka.filters.unsupervised.instance.Randomize();
                 myRandom.setInputFormat(insts);
                 insts = weka.filters.Filter.useFilter(insts, myRandom);
 
-                int trainSize = insts.numInstances() * percentSplit / 100;
+                int trainSize = insts.numInstances()*percentSplit/100;
                 int testSize = insts.numInstances() - trainSize;
 
-                Console.WriteLine("Scheme: "+cl.getClass());
+                Console.WriteLine("Scheme: " + cl.getClass());
                 Console.WriteLine("Relation: iris ");
                 Console.WriteLine("Instances: " + insts.numInstances());
                 Console.WriteLine("Attributes: " + insts.numAttributes());
-                Console.WriteLine("Train data instance: "+trainSize);
-                Console.WriteLine("Test data instance: "+testSize);
+                Console.WriteLine("Train data instance: " + trainSize);
+                Console.WriteLine("Test data instance: " + testSize);
                 weka.core.Instances train = new weka.core.Instances(insts, 0, trainSize);
 
                 cl.buildClassifier(train);
@@ -141,7 +176,7 @@ namespace TweetsRecommender
                         numCorrect++;
                 }
                 Console.WriteLine(numCorrect + " out of " + testSize + " correct (" +
-                           (double)((double)numCorrect / (double)testSize * 100.0) + "%)");
+                                  (double) ((double) numCorrect/(double) testSize*100.0) + "%)");
                 Console.ReadKey();
             }
             catch (java.lang.Exception ex)
@@ -150,6 +185,135 @@ namespace TweetsRecommender
             }
         }
 
-    }
-}
+        private static void AnalyzeTweetUsingPos(string tweet, TagType tagType = TagType.Noun)
+        {
+            try
+            {
 
+                //Tokenize tweet
+                
+                EnglishMaximumEntropyTokenizer tokenizer =
+                    new EnglishMaximumEntropyTokenizer(modelPath + "EnglishTok.nbin");
+                string[] tokens = tokenizer.Tokenize(tweet);
+
+                //Tag tokens
+                EnglishMaximumEntropyPosTagger posTagger =
+                    new EnglishMaximumEntropyPosTagger(modelPath + "EnglishPOS.nbin");
+                string[] tags = posTagger.Tag(tokens);
+                //Console.WriteLine(tokens.Length + " tokens found.");
+                //Console.WriteLine("Building tags. Please wait this might take a while...");
+                
+                int i = 0;
+               // int countTagged = 0;
+                foreach (string tag in tags)
+                {
+                    if (tagType == TagType.Noun) //tag noun
+                    {
+                        if (tag.Equals(NN) || tag.Equals(NNP) || tag.Equals(NNPS) || tag.Equals(NNS))
+                        {
+                            tagBuilder.AppendLine(tokens[i]);
+                           // countTagged++;
+                        }
+                    }
+                    else //tag verb
+                    {
+                        if (tag.Equals(VB) || tag.Equals(VBD) || tag.Equals(VBG) || tag.Equals(VBN)
+                            || tag.Equals(VBP) || tag.Equals(VBZ))
+                        {
+                            tagBuilder.AppendLine(tokens[i]);
+                           // countTagged++;
+                        }
+                    }
+                    i++;
+
+                }
+                //Console.WriteLine("Building tags done.");
+                //Console.WriteLine("Writing tags to file..");
+                //write tags to file
+                //if (!System.IO.Directory.Exists(@"C:\bigdata"))
+                //    System.IO.Directory.CreateDirectory(@"C\bigdata");
+                //System.IO.File.WriteAllText(@"C:\bigdata\tagslist.txt", sb.toString());
+                //Console.WriteLine(countTagged + " Tags successfully Written to file!");
+                //Console.Read();
+            }
+
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+
+        }
+
+        private static void AnalyzeTweetUsingPosTest(string tweet, TagType tagType = TagType.Noun)
+        {
+            try
+            {
+
+                //Tokenize tweet
+
+                EnglishMaximumEntropyTokenizer tokenizer =
+                    new EnglishMaximumEntropyTokenizer(modelPath + "EnglishTok.nbin");
+                string[] tokens = tokenizer.Tokenize(tweet);
+
+                //Tag tokens
+                EnglishMaximumEntropyPosTagger posTagger =
+                    new EnglishMaximumEntropyPosTagger(modelPath + "EnglishPOS.nbin");
+                string[] tags = posTagger.Tag(tokens);
+                Console.WriteLine(tokens.Length + " tokens found.");
+                Console.WriteLine("Building tags. Please wait this might take a while...");
+
+                int i = 0;
+                 int countTagged = 0;
+                foreach (string tag in tags)
+                {
+                    if (tagType == TagType.Noun) //tag noun
+                    {
+                        if (tag.Equals(NN) || tag.Equals(NNP) || tag.Equals(NNPS) || tag.Equals(NNS))
+                        {
+                            tagBuilder.AppendLine(tokens[i]);
+                             countTagged++;
+                        }
+                    }
+                    else //tag verb
+                    {
+                        if (tag.Equals(VB) || tag.Equals(VBD) || tag.Equals(VBG) || tag.Equals(VBN)
+                            || tag.Equals(VBP) || tag.Equals(VBZ))
+                        {
+                            tagBuilder.AppendLine(tokens[i]);
+                            countTagged++;
+                        }
+                    }
+                    i++;
+
+                }
+                Console.WriteLine("Building tags done.");
+                Console.WriteLine("Writing tags to file..");
+                //write tags to file
+                if (!System.IO.Directory.Exists(@"C:\bigdata"))
+                    System.IO.Directory.CreateDirectory(@"C\bigdata");
+                System.IO.File.WriteAllText(@"C:\bigdata\tagslist.txt", tagBuilder.toString());
+                Console.WriteLine(countTagged + " Tags successfully Written to file!");
+                Console.Read();
+            }
+
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+
+        }
+
+        enum TagType
+        {
+            Noun,
+            Verb
+        }
+    }
+
+}
